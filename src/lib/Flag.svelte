a32974fb5ea53471b12d884d815492d00d7e492f
Luciano Delucchi <luciano.delucchi@gmail.com>
2021-04-14 21:37:08 +0200

    refactor: create flag component

---
+++ src/lib/Flag.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { assets } from '$app/paths';
+
+    export let country;
+</script>
+
+<div class="flex justify-center">
+    <img src="{assets}/flags/svg/{country}.svg" class="lg:w-1/2 w-full" alt="Flag to guess..."/>
+</div>
--- src/routes/start.svelte
+++ src/routes/start.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-    import { assets } from '$app/paths';
-
+    import Flag from "$lib/Flag.svelte";
 </script>
-<div class="flex justify-center">
-    <img src="{assets}/flags/svg/AR.svg" class="lg:w-1/2 w-full"/>
-</div>
+<Flag country="AR"></Flag>
