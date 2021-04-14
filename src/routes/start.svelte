1836d44699900fb9d055398d3c1bc5b5e72329dc
Luciano Delucchi <luciano.delucchi@gmail.com>
2021-04-14 18:53:59 +0200

    feature: first version showing a hardcoded flag

--- src/routes/start.svelte
+++ src/routes/start.svelte
@@ -1,1 +1,7 @@
-<h1>Starting the game</h1>
+<script lang="ts">
+    import { assets } from '$app/paths';
+
+</script>
+<div class="flex justify-center">
+    <img src="{assets}/flags/svg/AR.svg" class="lg:w-1/2 w-full"/>
+</div>
