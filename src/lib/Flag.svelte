a0986de1aa8d3fc994596201a4ea838228cbb0f4
Luciano Delucchi <luciano.delucchi@gmail.com>
2021-04-16 15:11:17 +0200

    refactor: let container style flag component

--- src/lib/Flag.svelte
+++ src/lib/Flag.svelte
@@ -2,6 +2,4 @@
     export let countryCode: string;
 </script>
 
-<div class="flex justify-center">
-    <img src="/flags/svg/{countryCode.toLowerCase()}.svg" class="lg:w-1/2 w-full" alt="Flag to guess..."/>
-</div>
+<img src="/flags/svg/{countryCode.toLowerCase()}.svg" alt="Flag to guess..."/>
